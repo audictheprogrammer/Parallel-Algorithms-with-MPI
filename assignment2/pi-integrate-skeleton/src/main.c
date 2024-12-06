1a8a738f7579b6eb1e80664b8fe9d16a0d9ff4c8
audictheprogrammer <audicxu@gmail.com>
2024-12-06 21:02:14 +0100

    Assignment 2 - Completed Part2/2

--- assignment2/pi-integrate-skeleton/src/main.c
+++ assignment2/pi-integrate-skeleton/src/main.c
@@ -9,6 +9,8 @@
 #include <unistd.h>
 #include <limits.h>
 #include <float.h>
+#include <math.h>
+#include <mpi.h>
 
 #include "timing.h"
 
@@ -16,25 +18,48 @@ double integrate(double, double);
 
 int main (int argc, char** argv) {
     double wcs, wce;
-    double  Pi;
+    double Pi;
+    double a = 0, b = 1;
+    
+    MPI_Init(&argc, &argv);
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    wcs = getTimeStamp();
-    Pi = integrate(0.0, 1.0);
-    wce = getTimeStamp();
+    double a_local = a + rank * ((b-a) / size);
+    double b_local = a + (rank+1) * ((b-a) / size);
+
+    wcs = MPI_Wtime();
+    double Pi_loc = integrate(a_local, b_local);
+    MPI_Allreduce(&Pi_loc, &Pi, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    Pi *= 4;
+    wce = MPI_Wtime();
 
     printf("Pi=%.15lf in %.3lf s \n", Pi,wce-wcs);
+    MPI_Finalize();
     return EXIT_SUCCESS;
 }
 
+double f(double x) {
+    return sqrt(1 - pow(x, 2));
+}
+
 double integrate(double a, double b) {
-	
-	/*
-	
-		Your logic to integrate between given interval a to b.
-		Declare SLICES here and calculate delta x using a, b and SLICES.
-		Iterate over number of slices, calculate the area and sum them.
-		Return sum * delta x to get the value of PI.
-		
-	*/
-	return 0.0;
+	/* Your logic to integrate between given interval a to b.
+    Declare N here and calculate h using a, b and N.
+    Iterate over N, calculate the area and sum them.
+    Return sum * h to get the value of PI. */
+    int size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int N = 1e9;
+    double N_local = N / size;
+    double sum = 0.;
+    double h = (b-a) / N_local;
+
+    for (int i = 0; i < N_local; i++) {
+        sum += f(a + i*h);
+    }
+
+	return h*sum;
 }
